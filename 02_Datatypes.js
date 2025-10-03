8dcc6a835c20854ac575814309829c8616eda91f
Chetan2005-code <vchaitanya200511@gmail.com>
2025-10-03 14:30:45 +0000

    Datatypes of js

---
+++ 02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version 
+//alert(3+3)// we are using nodejs , not browser
+
+
+let name="chetan"//String
+let age=19//Number
+let isloggedIn = false//Boolean 
+
+/* Datatypes in Js 
+number => 2^53
+big-int
+String=""
+Boolean=> true/false
+null=> standalone value
+undefined=> value not given to variable 
+symbol mainly used in react(Unique wise used)
+Object
+
+*/
+console.log(typeof "chetan");//"the typeof" used to find datatype of variable 
+// Interview purpose Question
+console.log(typeof undefined);// the value you get is undefined 
+console.log(typeof null);// the value you get is object 
+
